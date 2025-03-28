1f68e5947313fcfa0a346797d4c8710dffe9f551
M-Ahmed-dev <winterfell551@gmail.com>
2025-03-28 12:28:39 +0500

    completed data privacy and sustainability routes

---
+++ src/app/(privacy-statement)/datenschutz/DataPrivacyDetails.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function DataPrivacyDetails({ data }) {
+  return (
+    <div>
+      {/* <!-- about section --> */}
+      <section id="flx-about" className="!tw-pt-10 ">
+        <div className="container ">
+          <div className="row">
+            <div className="col-12  flx-about-col pe-5">
+              <div className="text-left">
+                {/*<span className="flx-about-stock">Smoothies</span>*/}
+                <h1 className="hsn-smoothies">{data?.title}</h1>
+                <div className="float-end col-6 flx-forever-object">
+                  <div className="disk-container d-flex align-items-center justify-content-center">
+                    <div className="text-center">
+                      <h2 className="hsn-smoothies fs-2 fw-bold mb-1 lh-1">Indivit</h2>
+                      <p className="fs-6 mb-1 lh-1">
+                        Privatsphäre
+                        <br />
+                        Datenschutz
+                      </p>
+                    </div>
+                  </div>
+                  <div className="text-left flx-img-position">
+                    <Image
+                      src="/assets/img/indivit_data_privacy.png"
+                      alt=""
+                      height={500}
+                      width={500}
+                      className="img-fluid z-3"
+                    />
+                  </div>
+                </div>
+
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: typeof data?.description === 'string' ? data?.description : '',
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- about section --> */}
+    </div>
+  )
+}
--- src/app/(sustainability)/nachhaltige-bio-lebensmittel-essen/page.tsx
+++ src/app/(sustainability)/nachhaltige-bio-lebensmittel-essen/page.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import HeroBanner from '@/components/common/HeroBanner'
+import React from 'react'
+import SustainabilityDetails from './SustainabilityDetails'
+
 // Sustainability Page
-export default function page() {
-  return <div>Sustainability </div>;
+export default function Page() {
+  return (
+    <div>
+      <HeroBanner
+        data={{
+          title: 'Nachhaltigkeit liegt uns am Herzen',
+          description:
+            'Vieles von dem, was wir täglich umsetzen, möchten wir auch in den nächsten Jahren weiterführen. Daher liegt uns ein nachhaltiges Handeln sehr nahe. Das bedeutet für uns ganz konkret, wir positionieren uns für einen umfassenden Umweltschutz, ein vollständiges Recycling, einen sinnvollen Ressourceneinsatz und minimale Verschwendung.',
+        }}
+      />
+      <SustainabilityDetails />
+    </div>
+  )
 }
