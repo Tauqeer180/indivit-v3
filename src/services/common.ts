0975067949d3744f4f93100fa465badc649cf732
Tauqeer Abbas <47059409+Tauqeer180@users.noreply.github.com>
2025-05-08 19:57:47 +0500

    rebuild

--- src/services/common.ts
+++ src/services/common.ts
@@ -1,4 +1,4 @@
 import { fetcher } from "@/lib/fetcher";
 
 
-export const getSEOData = async (page) => await fetcher(`getSeoPageDetail?page_title=${page}`, { cache: true, revalidate: 3600*4 });
+export const getSEOData = async (page) => await fetcher(`getSeoPageDetail?page_title=${page}`, { cache: true, revalidate: 3600/2 });
