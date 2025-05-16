a027503e33317b4885acb8eb7beae153b5848c7c
Tauqeer Abbas <47059409+Tauqeer180@users.noreply.github.com>
2025-05-16 19:28:37 +0500

    blog schema

--- src/app/(blogs)/components/SEOSchema.tsx
+++ src/app/(blogs)/components/SEOSchema.tsx
@@ -1,35 +1,35 @@
 import { baseURL } from '@/lib/fetcher'
 import React from 'react'
 
-
 export default function SEOSchema({ data }) {
-  const schemas = {
-    '@context': 'https://schema.org',
-    '@type': 'BlogPosting',
-    mainEntityOfPage: {
-      '@type': 'WebPage',
-      '@id': `${baseURL}/blog/${data?.slug}`,
-    },
-    headline: data?.title,
-    description:
-      data?.short_text,
-    image: baseURL + 'blogs/' + data?.image,
-    author: {
-      '@type': 'Organization',
-      name: 'Indivit',
-      url: 'https://indivit.de',
-    },
-    publisher: {
-      '@type': 'Organization',
-      name: 'Indivit',
-      logo: {
-        '@type': 'ImageObject',
-        url: 'https://indivit.de/static/media/logo1.2f3c6f6529a9f1e4ab65.png',
+  const schemas = [
+    {
+      '@context': 'https://schema.org',
+      '@type': 'BlogPosting',
+      mainEntityOfPage: {
+        '@type': 'WebPage',
+        '@id': `${baseURL}/blog/${data?.slug}`,
       },
+      headline: data?.title,
+      description: data?.short_text,
+      image: baseURL + 'blogs/' + data?.image,
+      author: {
+        '@type': 'Organization',
+        name: 'Indivit',
+        url: 'https://indivit.de',
+      },
+      publisher: {
+        '@type': 'Organization',
+        name: 'Indivit',
+        logo: {
+          '@type': 'ImageObject',
+          url: 'https://indivit.de/static/media/logo1.2f3c6f6529a9f1e4ab65.png',
+        },
+      },
+      datePublished: data?.created_at,
+      dateModified: data?.updated_at,
     },
-    datePublished: data?.created_at,
-    dateModified: data?.updated_at,
-  }
+  ]
 
   return (
     <>
